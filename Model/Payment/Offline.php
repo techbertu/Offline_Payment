e6942efea1cdb2b9bb64549068a10c2187ca3771
techbertu <tech.bertu@gmail.com>
2020-05-11 16:15:22 +0200

    Add files via upload

--- Model/Payment/Offline.php
+++ Model/Payment/Offline.php
@@ -1,1 +1,17 @@
+<?php
 
+
+namespace TechBertu\OfflinePayment\Model\Payment;
+
+class Offline extends \Magento\Payment\Model\Method\AbstractMethod
+{
+
+    protected $_code = "offlinepayment";
+    protected $_isOffline = true;
+
+    public function isAvailable(
+        \Magento\Quote\Api\Data\CartInterface $quote = null
+    ) {
+        return parent::isAvailable($quote);
+    }
+}
